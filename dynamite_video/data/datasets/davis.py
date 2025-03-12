3d07222053d032002ac15818f8cf92022e22f760
Srijeet Roy <srijeet.11@gmail.com>
2025-03-12 12:11:02 +0100

    load annotations from JSON instead of PNGs

--- dynamite_video/data/datasets/davis.py
+++ dynamite_video/data/datasets/davis.py
@@ -1,4 +1,5 @@
 import os
+import json
 import numpy as np
 import pycocotools.mask as mt
 
@@ -15,7 +16,7 @@ class DAVISTrainingDataset(TrainingDataset):
     """
     DAVIS Training Dataset Class
 
-    Creates a `torch.utils.data.Dataset` class to load BURST dataset
+    Creates a `torch.utils.data.Dataset` class to load DAVIS dataset
     """
     
     def __init__(self, cfg, num_samples: int):
@@ -47,7 +48,8 @@ class DAVISTrainingDataset(TrainingDataset):
         
 
         # load masks of video frames in DAVIS training split 
-        annotations_content = self.map_annotations(path_to_images, Paths.to_davis_annotations(), Paths.to_davis_train_imset())
+        # annotations_content = self.map_annotations_IO(path_to_images, Paths.to_davis_annotations(), Paths.to_davis_train_imset())
+        annotations_content = self.map_annotations(path_to_images, Paths.to_davis_train_annotations_json())
         
         # cast each video sequence in the dataset to a generic `GenericVideoSequence` template
         videos, meta_info = parse_generic_video_dataset(path_to_images, annotations_content)
@@ -66,7 +68,204 @@ class DAVISTrainingDataset(TrainingDataset):
             self.fallback_candidates[num_instances].add(i)
 
 
-    # dynamite style
+    def map_annotations(
+            self,
+            path_to_images: str,
+            path_to_annotations: str, 
+    ):
+        """
+        Read semantic masks from PNG files
+
+        Args:
+            path_to_images: path to image directory
+            path_to_annotations: path to JSON annotations
+
+        Returns a dictionary with annotation content from the entire dataset
+        """
+        
+        # load the list of training sequences as a list
+        with open(path_to_annotations, 'r') as f:
+            content = json.load(f)
+
+        sequences = []
+
+        MIN_MASK_AREA = self.cfg.TRAINING.MIN_MASK_AREA
+        
+        # for each video sequence in the dataset
+        for seq in content["sequences"]:
+            
+            entry = {}
+            entry["id"] = f"{self.name}/{seq['id']}"
+            entry["dataset"] = self.name
+            entry["height"] = seq["height"]
+            entry["width"] = seq["width"]
+            img_dims = (seq['height'], seq['width'])
+
+            # relative paths to all JPEG images of the dataset (only the annotated ones)
+            entry["image_paths"] = [os.path.join(path_to_images, fpath) for fpath in seq["image_paths"]]
+
+            updated_segmentations = []
+            accepted_track_ids = set()
+
+            for fr_idx, segs_t in enumerate(seq['segmentations']):
+                updated_segmentations.append(dict())
+                for track_id, seg in segs_t.items():
+                    # only consider instances with a minimum mask area
+                    if self.mask_area(seg, img_dims) >= MIN_MASK_AREA:
+                        # store instance mask
+                        updated_segmentations[-1][int(track_id)] = seg
+                        accepted_track_ids.add(track_id)
+            
+            
+            # if none of the instances are large enough, exclude this sequence
+            if not accepted_track_ids:
+                continue
+
+            entry['segmentations'] = updated_segmentations
+            entry["categories"] = {
+                int(track_id): seq["categories"][track_id]
+                for track_id in accepted_track_ids
+            }
+            
+            sequences.append(entry)
+
+            # TODO - remove
+            if len(sequences)==2:
+                break
+
+        annotations_content = {}
+        # there is no explicit categories present in DAVIS
+        annotations_content["meta"] = {"category_labels": {1: 'object'}}
+        annotations_content["sequences"] = sequences
+        del content
+
+        return annotations_content
+    
+    def mask_area(self, rle, img_dims):
+        """
+        Area of an RLE segment
+        """
+        return mt.area({
+            "counts": rle.encode("utf-8"),
+            "size": img_dims
+        })
+    
+    
+    # def map_annotations_IO(
+    #         self,
+    #         path_to_images: str,
+    #         path_to_annotations: str, 
+    #         path_to_imset: str, 
+    # ):
+    #     """
+    #     Read semantic masks from PNG files
+
+    #     Args:
+    #         path_to_images: path to image directory
+    #         path_to_annotations: path to annotations directory
+    #         path_to_imset: path to imset .txt file, listing training sequence names
+
+    #     Returns a dictionary with annotation content from the entire dataset
+    #     """
+        
+    #     # load the list of training sequences as a list
+    #     with open(path_to_imset, 'r') as f:
+    #         sequences = [seq.rstrip() for seq in f.readlines()]
+
+    #     sequence_annotations = []
+
+    #     MIN_MASK_AREA = self.cfg.TRAINING.MIN_MASK_AREA
+        
+    #     # for each video sequence in the dataset
+    #     for seq in sequences:
+    #         entry = {}
+    #         entry["id"] = f"{self.name}/{seq}"
+    #         entry["dataset"] = self.name
+
+    #         # load paths to all image files of the sequence
+    #         imagefiles = sorted([os.path.join(path_to_images, seq, file) for file in os.listdir(os.path.join(path_to_images, seq)) if file.endswith('jpg')])
+    #         entry["image_paths"] = imagefiles
+            
+    #         # load paths to all mask files of a sequence
+    #         maskfiles = sorted([os.path.join(path_to_annotations, seq, file) for file in os.listdir(os.path.join(path_to_annotations, seq)) if file.endswith('png')])
+
+    #         # read first frame mask again to store resolution and instances
+    #         mask0 = np.asarray(Image.open(maskfiles[0]))
+    #         entry["height"], entry["width"] = mask0.shape
+
+    #         segmentations = []
+    #         seq_instances = []
+    #         # for mask of each frame of the video sequence
+    #         for idx, file in enumerate(maskfiles):
+    #             # read and store the semantic map
+    #             mask = np.asarray(Image.open(file).convert("P")).astype(dtype='uint8')
+    #             # find how many instances in the mask (excluding bg, value 0)
+    #             instances = list(np.unique(mask))[1:]
+
+    #             # extract and store binary masks of individual instances from semantic mask
+    #             binary_masks = {}
+    #             for i in instances:
+    #                 _m = (mask==i).astype(dtype='uint8')
+    #                 # check mask area
+    #                 if self.mask_area(_m) >= MIN_MASK_AREA:
+    #                     # if mask larger than threshold, keep it
+    #                     binary_masks[int(i)] = mt.encode(np.asfortranarray(_m))
+    #                     seq_instances.append(i)
+    #             segmentations.append(binary_masks)
+
+    #         seq_instances = set(seq_instances)
+    #         entry["categories"] = {int(k):1 for k in seq_instances}
+    #         entry['segmentations'] = segmentations
+
+    #         sequence_annotations.append(entry)
+
+    #         # TODO - remove
+    #         if len(sequences)==2:
+    #             break
+
+    #     annotations_content = {}
+    #     # there is no explicit categories present in DAVIS
+    #     annotations_content["meta"] = {"category_labels": {1: 'object'}}
+    #     annotations_content["sequences"] = sequence_annotations
+
+    #     return annotations_content
+
+    # def mask_area_IO(self, mask):
+    #     assert isinstance(mask, np.ndarray)
+    #     bin_mask = mask.astype('uint8')
+    #     assert list(np.unique(bin_mask))==[0,1]
+    #     return bin_mask.sum()
+
+
+class DAVISInferenceDataset(InferenceDataset):
+    
+    def __init__(self, cfg):
+        # number of frames in each training sample
+        clip_length = cfg.DATASETS.DAVIS.INFERENCE.CLIP_LENGTH
+        # video fps
+        fps = cfg.DATASETS.DAVIS.INFERENCE.FPS
+        # number of overlapping frames between clips
+        num_overlapping_frames = cfg.DATASETS.DAVIS.INFERENCE.FRAME_OVERLAP
+        split = cfg.DATASETS.DAVIS.INFERENCE.SPLIT
+        # DAVIS has only one "val" split
+        assert split=="val"
+        
+        super().__init__(cfg, "DAVIS", clip_length, fps, num_overlapping_frames, split)
+
+        # get paths
+        path_to_images = Paths.to_davis_images()
+        # `images_dir` could be an fpack file
+        if not os.path.exists(path_to_images):
+            path_to_images = f"{path_to_images}.fpack"
+            assert os.path.exists(path_to_images), f"Directory not found: {path_to_images}"
+        
+
+        # load masks of video frames in DAVIS training split 
+        annotations_content = self.map_annotations(path_to_images, Paths.to_davis_annotations(), Paths.to_davis_val_imset())
+
+        self.annotation_clips = self.create_inference_clips(annotations_content)
+
+
     def map_annotations(
             self,
             path_to_images: str,
@@ -84,13 +283,11 @@ class DAVISTrainingDataset(TrainingDataset):
         Returns a dictionary with annotation content from the entire dataset
         """
         
-        # load the list of training sequences as a list
+        # load the list of evaluation sequences as a list
         with open(path_to_imset, 'r') as f:
             sequences = [seq.rstrip() for seq in f.readlines()]
 
         sequence_annotations = []
-
-        MIN_MASK_AREA = self.cfg.TRAINING.MIN_MASK_AREA
         
         # for each video sequence in the dataset
         for seq in sequences:
@@ -123,10 +320,8 @@ class DAVISTrainingDataset(TrainingDataset):
                 for i in instances:
                     _m = (mask==i).astype(dtype='uint8')
                     # check mask area
-                    if self.mask_area(_m) >= MIN_MASK_AREA:
-                        # if mask larger than threshold, keep it
-                        binary_masks[int(i)] = mt.encode(np.asfortranarray(_m))
-                        seq_instances.append(i)
+                    binary_masks[int(i)] = mt.encode(np.asfortranarray(_m))['counts'].decode("utf-8")
+                    seq_instances.append(i)
                 segmentations.append(binary_masks)
 
             seq_instances = set(seq_instances)
@@ -136,7 +331,7 @@ class DAVISTrainingDataset(TrainingDataset):
             sequence_annotations.append(entry)
 
             # TODO - remove
-            if len(sequence_annotations) == 2:
+            if len(sequences)==2:
                 break
 
         annotations_content = {}
@@ -146,12 +341,4 @@ class DAVISTrainingDataset(TrainingDataset):
 
         return annotations_content
 
-    def mask_area(self, mask):
-        assert isinstance(mask, np.ndarray)
-        bin_mask = mask.astype('uint8')
-        assert list(np.unique(bin_mask))==[0,1]
-        return bin_mask.sum()
 
-
-class DAVISInferenceDataset(InferenceDataset):
-    ...
