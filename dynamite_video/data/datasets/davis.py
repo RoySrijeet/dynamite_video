b8315c22d49de32e310bece4ea2b8b7b565b7609
Srijeet Roy <srijeet.11@gmail.com>
2025-03-15 14:45:12 +0100

    add davis inference dataset

--- dynamite_video/data/datasets/davis.py
+++ dynamite_video/data/datasets/davis.py
@@ -7,10 +7,13 @@ from PIL import Image
 from typing import Any, Dict, List
 from collections import defaultdict
 
-from utils.paths import Paths
-from data.datasets.base import TrainingDataset, InferenceDataset
-from data.generic_video_parser import GenericVideoSequence, parse_generic_video_dataset
+from dynamite_video.utils.paths import Paths
+from dynamite_video.data.datasets.base import TrainingDataset, InferenceDataset
+from dynamite_video.data.generic_video_parser import GenericVideoSequence, parse_generic_video_dataset
+from dynamite_video.data.utils.data_utils import compute_resized_dims, resize_images, resize_masks
 
+
+########################### TRAINING DATASET ###########################
 
 class DAVISTrainingDataset(TrainingDataset):
     """
@@ -48,8 +51,11 @@ class DAVISTrainingDataset(TrainingDataset):
         
 
         # load masks of video frames in DAVIS training split 
-        # annotations_content = self.map_annotations_IO(path_to_images, Paths.to_davis_annotations(), Paths.to_davis_train_imset())
         annotations_content = self.map_annotations(path_to_images, Paths.to_davis_train_annotations_json())
+        
+        # load masks from PNG files and store them as RLEs
+        # annotations_content = self.map_annotations_IO(path_to_images, Paths.to_davis_annotations(), Paths.to_davis_train_imset())
+        
         
         # cast each video sequence in the dataset to a generic `GenericVideoSequence` template
         videos, meta_info = parse_generic_video_dataset(path_to_images, annotations_content)
@@ -151,129 +157,17 @@ class DAVISTrainingDataset(TrainingDataset):
         })
     
     
-    # def map_annotations_IO(
-    #         self,
-    #         path_to_images: str,
-    #         path_to_annotations: str, 
-    #         path_to_imset: str, 
-    # ):
-    #     """
-    #     Read semantic masks from PNG files
-
-    #     Args:
-    #         path_to_images: path to image directory
-    #         path_to_annotations: path to annotations directory
-    #         path_to_imset: path to imset .txt file, listing training sequence names
-
-    #     Returns a dictionary with annotation content from the entire dataset
-    #     """
-        
-    #     # load the list of training sequences as a list
-    #     with open(path_to_imset, 'r') as f:
-    #         sequences = [seq.rstrip() for seq in f.readlines()]
-
-    #     sequence_annotations = []
-
-    #     MIN_MASK_AREA = self.cfg.TRAINING.MIN_MASK_AREA
-        
-    #     # for each video sequence in the dataset
-    #     for seq in sequences:
-    #         entry = {}
-    #         entry["id"] = f"{self.name}/{seq}"
-    #         entry["dataset"] = self.name
-
-    #         # load paths to all image files of the sequence
-    #         imagefiles = sorted([os.path.join(path_to_images, seq, file) for file in os.listdir(os.path.join(path_to_images, seq)) if file.endswith('jpg')])
-    #         entry["image_paths"] = imagefiles
-            
-    #         # load paths to all mask files of a sequence
-    #         maskfiles = sorted([os.path.join(path_to_annotations, seq, file) for file in os.listdir(os.path.join(path_to_annotations, seq)) if file.endswith('png')])
-
-    #         # read first frame mask again to store resolution and instances
-    #         mask0 = np.asarray(Image.open(maskfiles[0]))
-    #         entry["height"], entry["width"] = mask0.shape
-
-    #         segmentations = []
-    #         seq_instances = []
-    #         # for mask of each frame of the video sequence
-    #         for idx, file in enumerate(maskfiles):
-    #             # read and store the semantic map
-    #             mask = np.asarray(Image.open(file).convert("P")).astype(dtype='uint8')
-    #             # find how many instances in the mask (excluding bg, value 0)
-    #             instances = list(np.unique(mask))[1:]
-
-    #             # extract and store binary masks of individual instances from semantic mask
-    #             binary_masks = {}
-    #             for i in instances:
-    #                 _m = (mask==i).astype(dtype='uint8')
-    #                 # check mask area
-    #                 if self.mask_area(_m) >= MIN_MASK_AREA:
-    #                     # if mask larger than threshold, keep it
-    #                     binary_masks[int(i)] = mt.encode(np.asfortranarray(_m))
-    #                     seq_instances.append(i)
-    #             segmentations.append(binary_masks)
-
-    #         seq_instances = set(seq_instances)
-    #         entry["categories"] = {int(k):1 for k in seq_instances}
-    #         entry['segmentations'] = segmentations
-
-    #         sequence_annotations.append(entry)
-
-    #         # TODO - remove
-    #         if len(sequences)==2:
-    #             break
-
-    #     annotations_content = {}
-    #     # there is no explicit categories present in DAVIS
-    #     annotations_content["meta"] = {"category_labels": {1: 'object'}}
-    #     annotations_content["sequences"] = sequence_annotations
-
-    #     return annotations_content
-
-    # def mask_area_IO(self, mask):
-    #     assert isinstance(mask, np.ndarray)
-    #     bin_mask = mask.astype('uint8')
-    #     assert list(np.unique(bin_mask))==[0,1]
-    #     return bin_mask.sum()
-
-
-class DAVISInferenceDataset(InferenceDataset):
-    
-    def __init__(self, cfg):
-        # number of frames in each training sample
-        clip_length = cfg.DATASETS.DAVIS.INFERENCE.CLIP_LENGTH
-        # video fps
-        fps = cfg.DATASETS.DAVIS.INFERENCE.FPS
-        # number of overlapping frames between clips
-        num_overlapping_frames = cfg.DATASETS.DAVIS.INFERENCE.FRAME_OVERLAP
-        split = cfg.DATASETS.DAVIS.INFERENCE.SPLIT
-        # DAVIS has only one "val" split
-        assert split=="val"
-        
-        super().__init__(cfg, "DAVIS", clip_length, fps, num_overlapping_frames, split)
-
-        # get paths
-        path_to_images = Paths.to_davis_images()
-        # `images_dir` could be an fpack file
-        if not os.path.exists(path_to_images):
-            path_to_images = f"{path_to_images}.fpack"
-            assert os.path.exists(path_to_images), f"Directory not found: {path_to_images}"
-        
-
-        # load masks of video frames in DAVIS training split 
-        annotations_content = self.map_annotations(path_to_images, Paths.to_davis_annotations(), Paths.to_davis_val_imset())
-
-        self.annotation_clips = self.create_inference_clips(annotations_content)
-
-
-    def map_annotations(
+    def map_annotations_IO(
             self,
             path_to_images: str,
             path_to_annotations: str, 
             path_to_imset: str, 
     ):
         """
-        Read semantic masks from PNG files
+        Read semantic masks from PNG files.
+
+        NOTE: Considerably slower as involves reading PNG files and converting them
+        to RLEs
 
         Args:
             path_to_images: path to image directory
@@ -283,11 +177,13 @@ class DAVISInferenceDataset(InferenceDataset):
         Returns a dictionary with annotation content from the entire dataset
         """
         
-        # load the list of evaluation sequences as a list
+        # load the list of training sequences as a list
         with open(path_to_imset, 'r') as f:
             sequences = [seq.rstrip() for seq in f.readlines()]
 
         sequence_annotations = []
+
+        MIN_MASK_AREA = self.cfg.TRAINING.MIN_MASK_AREA
         
         # for each video sequence in the dataset
         for seq in sequences:
@@ -320,8 +216,10 @@ class DAVISInferenceDataset(InferenceDataset):
                 for i in instances:
                     _m = (mask==i).astype(dtype='uint8')
                     # check mask area
-                    binary_masks[int(i)] = mt.encode(np.asfortranarray(_m))['counts'].decode("utf-8")
-                    seq_instances.append(i)
+                    if self.mask_area_array(_m) >= MIN_MASK_AREA:
+                        # if mask larger than threshold, keep it
+                        binary_masks[int(i)] = mt.encode(np.asfortranarray(_m))
+                        seq_instances.append(i)
                 segmentations.append(binary_masks)
 
             seq_instances = set(seq_instances)
@@ -342,3 +240,158 @@ class DAVISInferenceDataset(InferenceDataset):
         return annotations_content
 
 
+    def mask_area_array(self, mask):
+        assert isinstance(mask, np.ndarray)
+        bin_mask = mask.astype('uint8')
+        assert list(np.unique(bin_mask))==[0,1]
+        return bin_mask.sum()
+
+
+########################### INFERENCE DATASET ###########################
+
+
+class DAVISInferenceDataset(InferenceDataset):
+    
+    def __init__(self, cfg):
+        # number of frames in each training sample
+        clip_length = cfg.DATASETS.DAVIS.INFERENCE.CLIP_LENGTH
+        # video fps
+        fps = cfg.DATASETS.DAVIS.INFERENCE.FPS
+        # number of overlapping frames between clips
+        num_overlapping_frames = cfg.DATASETS.DAVIS.INFERENCE.FRAME_OVERLAP
+        split = cfg.DATASETS.DAVIS.INFERENCE.SPLIT
+        # DAVIS has only one "val" split
+        assert split=="val"
+        
+        super().__init__(cfg, "DAVIS", clip_length, fps, num_overlapping_frames, split)
+
+        # get paths
+        self.path_to_images = Paths.to_davis_images()
+        # `images_dir` could be an fpack file
+        if not os.path.exists(self.path_to_images):
+            self.path_to_images = f"{self.path_to_images}.fpack"
+            assert os.path.exists(self.path_to_images), f"Directory not found: {self.path_to_images}"
+
+        self.path_to_annotations = Paths.to_davis_annotations()
+        self.path_to_val_imset = Paths.to_davis_val_imset()
+        
+        return self.create_inference_dataset()
+
+    
+    def create_inference_dataset(self):
+        """
+        Prepare dataset for evaluation. Involves the following steps:
+            1. Load images and masks from files
+            2. Extract clip metadata from the sequence
+        """
+
+        # load the list of evaluation sequences as a list
+        with open(self.path_to_val_imset, 'r') as f:
+            sequences = [seq.rstrip() for seq in f.readlines()]
+
+        
+        sequence_annotations = []
+        
+        for seq in sequences:
+            
+            metadata = {"id": f"{self.name}/{seq}"}
+
+            # load images
+            image_filepaths = sorted([os.path.join(self.path_to_images, seq, file) for file in os.listdir(os.path.join(self.path_to_images, seq)) if file.endswith('jpg')])
+            images = self.load_images(image_filepaths)      # [T, H, W, 3]
+
+            metadata["length"] = len(image_filepaths)
+            metadata["orig_dims"] = (images.shape[1], images.shape[2])
+
+            # semantic_maps - [T,H,W] np.ndarray
+            # bg_masks - [T,H,W] np.ndarray binary background masks
+            # instances_per_frame - list of IDs of instances present in each frame
+            # instance_discovery - dict, instance ID and frame index where the instance first appeared
+            mask_filepaths = sorted([os.path.join(self.path_to_annotations, seq, file) for file in os.listdir(os.path.join(self.path_to_annotations, seq)) if file.endswith('png')])
+            semantic_maps, bg_masks, instances_per_frame, instance_discovery = self.load_png_masks(mask_filepaths)
+            
+            # resize
+            if self.cfg.INPUT.AUGMENTATION.RESIZE_TEST:
+                # compute target resolution
+                new_height, new_width = compute_resized_dims(
+                    *images.shape[1:3], 
+                    min_dim=self.cfg.INPUT.AUGMENTATION.MIN_DIM_TEST,
+                    max_dim=self.cfg.INPUT.AUGMENTATION.MAX_DIM_TEST,
+                )
+                if (new_height, new_width) != metadata["orig_dims"]:
+                    images = resize_images(images, new_height, new_width)
+                    semantic_maps = resize_masks(semantic_maps, new_height, new_width, binary=False)
+                    bg_masks = resize_masks(bg_masks, new_height, new_width, binary=False)
+            
+            # arrange dimensions
+            images = np.transpose(images, (0, 3, 1, 2))   # [T, H, W, 3] -> [T, 3, H, W]
+            if self.cfg.INPUT.RGB:
+                # BGR -> RGB (load_images uses cv2.imread which reads images in BGR mode by default)
+                images = np.flip(images, 1).copy()
+
+            
+            # serialize instance IDs
+            # IDs of all instances present in the sequence
+            orig_instance_ids = sorted(list(instance_discovery.keys()))
+            orig_to_serial_ids, serial_to_orig_ids = self.serialize_instance_ids(orig_instance_ids)
+            assert orig_instance_ids == list(orig_to_serial_ids.keys())
+            metadata["orig_to_serial_ids"] = orig_to_serial_ids
+            metadata["serial_to_orig_ids"] = serial_to_orig_ids
+
+            metadata["instance_discovery"] = {orig_to_serial_ids[inst_id]: fr_idx 
+                                              for inst_id, fr_idx in instance_discovery.items()}
+            
+            
+            # extract binary instance masks from semantic map of each frame
+            sequence_instances_serial_ids = sorted(list(metadata["instance_discovery"].keys()))
+            instance_masks = []
+            for fr_idx, fr_map in enumerate(semantic_maps):
+                fr_inst_masks = []
+                for inst_id in sequence_instances_serial_ids:
+                    # semantic map is still labeled with original instance IDs
+                    orig_inst_id = serial_to_orig_ids[inst_id]
+                    if orig_inst_id in instances_per_frame[fr_idx]:
+                        fr_inst_masks.append((fr_map == orig_inst_id).astype('uint8'))
+                    else:
+                        # if an instance is absent, pad it with empty mask
+                        fr_inst_masks.append(np.zeros_like(fr_map).astype('uint8'))
+                instance_masks.append(np.stack(fr_inst_masks))
+            
+            instance_masks = np.stack(instance_masks)    # [T,N,H,W]
+
+            # recreate semantic maps with serial instance IDs
+            semantic_maps_serial = []
+            for fr_idx, fr_inst_masks in enumerate(instance_masks):
+                fr_map = np.zeros_like(fr_inst_masks[0])
+                for inst_id, inst_mask in zip(sequence_instances_serial_ids, fr_inst_masks):
+                    fr_map[np.where(inst_mask==1)] = inst_id
+                semantic_maps_serial.append(fr_map.astype('uint8'))
+
+                instances_per_frame[fr_idx] = [orig_to_serial_ids[orig_inst_id] for orig_inst_id in instances_per_frame[fr_idx]]
+
+            semantic_maps = np.stack(semantic_maps_serial)
+
+            metadata["images"] = images
+            metadata["instance_masks"] = instance_masks
+            metadata["semantic_maps"] = semantic_maps
+            metadata["bg_masks"] = bg_masks
+            metadata["instances_per_frame"] = instances_per_frame
+
+            # padding - there's no padding applied
+            padding_mask = np.zeros((images.shape[2], images.shape[3])).astype('uint8')
+            metadata["padding_mask"] = padding_mask
+
+            # clip indices
+            indices = []
+            step = self.clip_length - self.num_overlapping_frames
+            start = 0
+            while start + self.clip_length <= metadata["length"]:
+                indices.append(tuple(range(start, start + self.clip_length)))
+                start += step
+            if indices[-1][-1] != metadata["length"] - 1:
+                indices.append(tuple(range(indices[-1][-1] - self.num_overlapping_frames +1, metadata["length"])))
+            metadata["indices"] = indices
+
+            sequence_annotations.append(metadata)
+
+        return sequence_annotations
