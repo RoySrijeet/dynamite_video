51bd3a639719674445f6c197ac29d01eb9a55015
Srijeet Roy <srijeet.11@gmail.com>
2025-04-04 11:20:45 +0200

    add contiguous p.e. code

--- dynamite_video/model/interactive_transformer/position_encoding.py
+++ dynamite_video/model/interactive_transformer/position_encoding.py
@@ -15,6 +15,16 @@ class PositionEmbeddingSine(nn.Module):
     """
 
     def __init__(self, num_pos_feats=64, temperature=10000, normalize=False, scale=None):
+        """
+        Args:
+            num_pos_feats: Number of positional features. Typically, it is set to half of the total 
+                    embedding dimension since we later concatenate sine and cosine terms. Default: 64
+            temperature: hyperparameter for temperature scaling. Default: 10000
+            normalize: whether to scale the embeddings to the range [0,scale], ensuring they stay 
+                    bounded. Default: False
+            scale: scaling factor, if normalization is used. Default: None
+
+        """
         super().__init__()
         self.num_pos_feats = num_pos_feats
         self.temperature = temperature
@@ -25,10 +35,70 @@ class PositionEmbeddingSine(nn.Module):
             scale = 2 * math.pi
         self.scale = scale
 
-    def forward(self, x, mask=None):
+
+    def forward_contiguous(self, x, mask=None):
+        """
+        Compute contiguous positional embedding across all images in the batch, 
+        i.e., all frames of a clip
+
+        Args:
+            x: feature map of shape [T, D, h, w]
+            mask: determines which spatial locations are valid for positional embedding computation
+        
+        Return:
+            pos: positional embedding [Th, w, D]
+        """
+        if mask is None:
+            mask = torch.zeros((x.size(0) * x.size(2), x.size(3)), device=x.device, dtype=torch.bool)   # Thxw
+        not_mask = ~mask
+
+        # relative spatial coordinates for each pixel
+        y_embed = not_mask.cumsum(0, dtype=torch.float32)
+        x_embed = not_mask.cumsum(1, dtype=torch.float32)
+
+        if self.normalize:
+            eps = 1e-6
+            y_embed = y_embed / (y_embed[-1:, :] + eps) * self.scale
+            x_embed = x_embed / (x_embed[:, -1:] + eps) * self.scale
+
+        # scaling factor with exponential decay based on temperature
+        dim_t = torch.arange(self.num_pos_feats, dtype=torch.float32, device=x.device)
+        dim_t = self.temperature ** (2 * torch.div(dim_t, 2, rounding_mode='floor') / self.num_pos_feats)
+
+        pos_x = x_embed[:, :, None] / dim_t
+        pos_y = y_embed[:, :, None] / dim_t
+        
+        pos_x = torch.stack(
+            (pos_x[:, :, 0::2].sin(), pos_x[:, :, 1::2].cos()), dim=3       # Th, D/2, w
+        ).flatten(2)
+        pos_y = torch.stack(
+            (pos_y[:, :, 0::2].sin(), pos_y[:, :, 1::2].cos()), dim=3       # Th, D/2, w
+        ).flatten(2)
+
+        pos = torch.cat((pos_y, pos_x), dim=2)  # Th, D, w
+        return pos
+    
+    
+    def forward(self, x, contiguous=False, mask=None):
+        """
+        Compute positional embeddings for a 2D feature map
+
+        Args:
+            x: feature map of shape [T, D, h, w]
+            contiguous: whether to generate conntiguous p.e. across frames of the batch
+            mask: determines which spatial locations are valid for positional embedding computation
+
+        Return:
+            pos: positional embedding [T, D, h, w]
+        """
+        if contiguous:
+            return self.forward_contiguous(x, mask)
+
         if mask is None:
             mask = torch.zeros((x.size(0), x.size(2), x.size(3)), device=x.device, dtype=torch.bool)
         not_mask = ~mask
+
+        # relative spatial coordinates for each pixel
         y_embed = not_mask.cumsum(1, dtype=torch.float32)
         x_embed = not_mask.cumsum(2, dtype=torch.float32)
         if self.normalize:
@@ -49,6 +119,7 @@ class PositionEmbeddingSine(nn.Module):
         ).flatten(3)
         pos = torch.cat((pos_y, pos_x), dim=3).permute(0, 3, 1, 2)
         return pos
+
     
     def __repr__(self, _repr_indent=4):
         head = "Positional encoding " + self.__class__.__name__
