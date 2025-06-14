fix attention mask, ignore ignore mask, fix qqca p.e.

dynamite_video/model/dynamite_model.py
@@ -121,6 +121,8 @@ class DynamiteModel(nn.Module):
             fg_coords = None, 
             bg_coords = None, 
             max_timestamp=None,
+            visualize=None,
+            train_iter=None
     ):
         """
         Forward pass through the DynaMITe model
@@ -162,6 +164,13 @@ class DynamiteModel(nn.Module):
             # prepare ground truth mask information
             targets = self.prepare_targets(inputs)
 
+            if visualize:
+                import os
+                visualize_dir = "/home/roy/REPOS/dynamite_video/debug/visualization/training/dynamite_model_forward"
+                torch.save(inputs, os.path.join(visualize_dir, "input", f"inputs_iter_{train_iter}.pth"))
+                torch.save(features, os.path.join(visualize_dir, "features", f"features_iter_{train_iter}.pth"))
+                torch.save(targets, os.path.join(visualize_dir, "targets", f"targets_iter_{train_iter}.pth"))
+
             for clip_idx in range(len(inputs)):
             
                 clip_mask_features = mask_features[clip_idx] if mask_features is not None else None
@@ -176,7 +185,9 @@ class DynamiteModel(nn.Module):
                                                                                 num_clicks_per_object[clip_idx],
                                                                                 fg_coords[clip_idx],
                                                                                 bg_coords[clip_idx],
-                                                                                max_timestamp[clip_idx]
+                                                                                max_timestamp[clip_idx],
+                                                                                visualize=visualize,
+                                                                                train_iter=train_iter,
                                                                             )
                 
                 losses = self.criterion(clip_outputs, targets[clip_idx], clip_num_queries_per_object)

dynamite_video/model/interactive_transformer/decoder.py
@@ -50,11 +50,11 @@ class Decoder(nn.Module):
 
         #output is QxC
         for i in range(self.num_layers):
-            mask_features = self.cross_attention_layers[i](
+            mask_features, weights = self.cross_attention_layers[i](
                 mask_features, output,
                 memory_mask=None,
                 memory_key_padding_mask=None, 
                 pos=query_embed, query_pos=pos_encodings
             )
             mask_features = self.ffn_layers[i](mask_features)
-        return mask_features
+        return mask_features, weights

dynamite_video/model/interactive_transformer/descriptor_initializer.py
@@ -69,7 +69,7 @@ class AvgClicksPoolingInitializer(nn.Module):
         
         Returns:
             descriptors: query descriptors of input clicks
-            normalized_clicks_coords: space-time normalized clicks corresponding to each descriptor
+            normalized_clicks_w_learnables: space-time normalized clicks corresponding to each descriptor
         """
 
         norm_h, norm_w, norm_t = norms
@@ -82,55 +82,53 @@ class AvgClicksPoolingInitializer(nn.Module):
         H = float(h*8)
         W = float(w*8)
         
-        descriptors = []
-        normalized_clicks = []
+        descriptors = [[] for _ in range(T)]
+        normalized_clicks = [[] for _ in range(T)]
         num_queries_per_object = np.zeros((T, N+1)).astype('int')
-        
-        # stack queries for each frame
-        for fr_idx, fr_fg_coords in enumerate(batched_fg_coords_list):
 
-            # foreground queries of current frame
-            fr_fg_queries = []
-            fr_fg_normalized_clicks = []
-            
-            # stack queries for each instance in the frame
-            for inst_id, inst_fg_coords in enumerate(fr_fg_coords):
+        # obtain queries one instance at a time, across all frames
+        for inst_id in range(N):
+            for fr_idx, fr_fg_coords in enumerate(batched_fg_coords_list):
+                inst_fg_coords = fr_fg_coords[inst_id]
                 
-                # always add a learnable query
-                fr_fg_queries.append(repeat(self.no_click_query, "1 C -> 1 1 C"))
-                fr_fg_normalized_clicks.append(torch.tensor([-1.0, -1.0, -1.0]))
-                num_queries_per_object[fr_idx][inst_id] += 1
-
                 if len(inst_fg_coords) > 0:
-                    for coords in inst_fg_coords:
-                        fr_fg_normalized_clicks.append(torch.tensor([coords[0]/norm_h, coords[1]/norm_w, coords[-1]/norm_t]))
-                        num_queries_per_object[fr_idx][inst_id] += 1
+                    # for each click on the instance in the current frame
+                    # add a learnable query in the other frames of the clip
+                        
+                    for fr, desc, clks in zip(range(T), descriptors, normalized_clicks):
+                        if fr==fr_idx:
+                            for coords in inst_fg_coords:
+                                # normalize the click
+                                clks.append(torch.tensor([coords[0]/norm_h, coords[1]/norm_w, coords[2], coords[3]/T, coords[-1]/norm_t]))
+                                num_queries_per_object[fr_idx][inst_id] += 1
 
-                    clicks = torch.tensor(inst_fg_coords, dtype=torch.float, device=device)
-                    # extract and scale spatial coordinates
-                    clicks = clicks[:,:2]
-                    clicks[:,0]/=H
-                    clicks[:,1]/=W
-                    # invert (y,x) -> (x,y)
-                    clicks = clicks.flip(-1)
-                    
-                    inst_queries = []
-                    # extract click features in each scale of multi-res features
-                    for i in range(feature_levels):
-                        # feature maps at i-th feature scale
-                        fmap_scale = features[i]
-                        # map of particular frame at i-th feature scale
-                        fmap_scale_fr = fmap_scale[fr_idx].unsqueeze(0)
+                            # extract query descriptor
+                            clicks = torch.tensor(inst_fg_coords, dtype=torch.float, device=device)
+                            clicks = clicks[:,:2]
+                            clicks[:,0]/=H
+                            clicks[:,1]/=W
+                            # invert (y,x) -> (x,y)
+                            clicks = clicks.flip(-1)
+                            
+                            inst_queries = []
+                            # extract click features in each scale of multi-res features
+                            for i in range(feature_levels):
+                                # feature maps at i-th feature scale
+                                fmap_scale = features[i]
+                                # map of particular frame at i-th feature scale
+                                fmap_scale_fr = fmap_scale[fr_idx].unsqueeze(0)
 
-                        nbd_features = self.get_features_descriptors(fmap_scale_fr, clicks.unsqueeze(0))    # 1,1,D
-                        inst_queries.append(nbd_features)
+                                nbd_features = self.get_features_descriptors(fmap_scale_fr, clicks.unsqueeze(0))    # 1,1,D
+                                inst_queries.append(nbd_features)
 
-                    # take the average of the features from multiple scales as the query for the clicks on this instance
-                    avg_inst_query = torch.mean(torch.stack(inst_queries, -1), dim = -1)
-                    fr_fg_queries.append(avg_inst_query)
-            
-            descriptors.append(fr_fg_queries)
-            normalized_clicks.append(fr_fg_normalized_clicks)
+                            # take the average of the features from multiple scales as the click query
+                            avg_inst_query = torch.mean(torch.stack(inst_queries, -1), dim = -1)
+                            desc.extend(torch.split(avg_inst_query, 1, dim=1))
+                        else:
+                            for coords in inst_fg_coords:
+                                desc.append(repeat(self.no_click_query, "1 C -> 1 1 C"))
+                                clks.append(torch.tensor([coords[0]/norm_h, coords[1]/norm_w, coords[2], fr/T, coords[-1]/norm_t]))
+                                num_queries_per_object[fr][inst_id] += 1
         
         # background queries
         for fr_idx, fr_bg_coords in enumerate(batched_bg_coords_list):
@@ -139,110 +137,47 @@ class AvgClicksPoolingInitializer(nn.Module):
                 continue
             
             for coords in fr_bg_coords:
-                normalized_clicks[fr_idx].append(torch.tensor([coords[0]/norm_h, coords[1]/norm_w, coords[-1]/norm_t]))
-                num_queries_per_object[fr_idx][-1] += 1
+                for fr, desc, clks in zip(range(T), descriptors, normalized_clicks):
+                    if fr==fr_idx:
+                        clks.append(torch.tensor([coords[0]/norm_h, coords[1]/norm_w, coords[2], coords[3]/T, coords[-1]/norm_t]))
+                        num_queries_per_object[fr_idx][-1] += 1
             
-            clicks = torch.tensor(fr_bg_coords, dtype=torch.float, device=device)
-            # extract and scale spatial coordinates
-            clicks = clicks[:,:2]
-            clicks[:,0]/=H
-            clicks[:,1]/=W
-            # invert (y,x) -> (x,y)
-            clicks = clicks.flip(-1)
+                        clicks = torch.tensor(fr_bg_coords, dtype=torch.float, device=device)
+                        # extract and scale spatial coordinates
+                        clicks = clicks[:,:2]
+                        clicks[:,0]/=H
+                        clicks[:,1]/=W
+                        # invert (y,x) -> (x,y)
+                        clicks = clicks.flip(-1)
 
-            fr_bg_queries = []
-            for i in range(feature_levels):
-                # maps at i-th feature scale
-                fmap_scale = features[i]
-                # map of particular frame at i-th feature scale
-                fmap_scale_fr = fmap_scale[fr_idx].unsqueeze(0)
+                        fr_bg_queries = []
+                        for i in range(feature_levels):
+                            # maps at i-th feature scale
+                            fmap_scale = features[i]
+                            # map of particular frame at i-th feature scale
+                            fmap_scale_fr = fmap_scale[fr_idx].unsqueeze(0)
 
-                nbd_features = self.get_features_descriptors(fmap_scale_fr, clicks.unsqueeze(0))
-                fr_bg_queries.append(nbd_features)
-            
-            avg_bg_query = torch.mean(torch.stack(fr_bg_queries, -1), dim = -1)
-            descriptors[fr_idx].append(avg_bg_query)
+                            nbd_features = self.get_features_descriptors(fmap_scale_fr, clicks.unsqueeze(0))
+                            fr_bg_queries.append(nbd_features)
+                        
+                        avg_bg_query = torch.mean(torch.stack(fr_bg_queries, -1), dim = -1)
+                        desc.extend(torch.split(avg_bg_query, 1, dim=1))
+                    else:
+                        for coords in inst_fg_coords:
+                            desc.append(repeat(self.no_click_query, "1 C -> 1 1 C"))
+                            clks.append(torch.tensor([coords[0]/norm_h, coords[1]/norm_w, coords[2], fr/T, coords[-1]/norm_t]))
+                            num_queries_per_object[fr][-1] += 1
 
         # at this point, in each frame, there is at least one query for 
-        # each instance present in that frame
-        descriptors = [
-                        torch.cat(desc, dim=1) if len(desc) > 0 else torch.zeros((1, 0, self.hidden_dim), device=device)
-                        for desc in descriptors
-                    ]
+        # each object present in that frame. Each click query in one frame 
+        # has corresponding learnable queries in the other frames
+        descriptors = [torch.cat(desc, dim=1) for desc in descriptors]
+        descriptors = torch.cat(descriptors, dim=0)
         
-        return descriptors, normalized_clicks, num_queries_per_object.tolist()
-
-
-        # NOTE: the implementation below is slower - bg clicks are empty more often than not
-        # for fr_idx, fr_fg_coords, fr_bg_coords in zip(range(T), batched_fg_coords_list, batched_bg_coords_list):
-
-        #     fr_descriptors = []
-        #     normalized_clicks.append([])
-        #     clicks = []
-        #     insert_index = []
-            
-        #     # stack queries for each object in the frame
-        #     for inst_id, inst_fg_coords in enumerate(fr_fg_coords):
-                
-        #         # always add a learnable query for each object
-        #         fr_descriptors.append(repeat(self.no_click_query, "1 C -> 1 1 C"))
-        #         normalized_clicks[-1].append(torch.tensor([-1.0, -1.0, -1.0]))
-        #         num_queries_per_object[fr_idx][inst_id] += 1
-
-        #         # collect fg clicks, if available
-        #         if len(inst_fg_coords) == 0:
-        #             continue
-                
-        #         # each click is normalized, and corresponding point features are computed once per frame (along with bg) later
-        #         clicks.extend(inst_fg_coords)
-        #         for coords in inst_fg_coords:
-        #             insert_index.append(len(fr_descriptors))
-        #             fr_descriptors.append([])
-        #             normalized_clicks[-1].append(torch.tensor([coords[0]/norm_h, coords[1]/norm_w, coords[-1]/norm_t]))
-        #             num_queries_per_object[fr_idx][inst_id] += 1
-                
-        #     # collect bg clicks
-        #     if len(fr_bg_coords) > 0:
-        #         clicks.extend(fr_bg_coords)
-        #         for coords in fr_bg_coords:
-        #             insert_index.append(len(fr_descriptors))
-        #             fr_descriptors.append([])
-        #             normalized_clicks[-1].append(torch.tensor([coords[0]/norm_h, coords[1]/norm_w, coords[-1]/norm_t]))
-        #             num_queries_per_object[fr_idx][-1] += 1
-
-        #     if len(clicks) > 0:
-            
-        #         # `clicks` contains both fg and bg clicks
-        #         clicks = torch.tensor(clicks, dtype=torch.float, device=device)
-        #         # extract and scale spatial coordinates
-        #         clicks = clicks[:,:2]
-        #         clicks[:,0]/=H
-        #         clicks[:,1]/=W
-        #         # invert (y,x) -> (x,y)
-        #         clicks = clicks.flip(-1)
-                
-        #         fr_queries_per_scale = []
-        #         # extract click features in each scale of multi-res features
-        #         for i in range(feature_levels):
-        #             # feature map of particular frame at i-th scale
-        #             fmap_scale_fr = features[i][fr_idx].unsqueeze(0)
-        #             # point features at current scale
-        #             nbd_features = point_sample(fmap_scale_fr, clicks.unsqueeze(0), align_corners=False)
-        #             nbd_features = torch.permute(nbd_features, (0, 2, 1))
-        #             # split per query
-        #             fr_queries_per_scale.append(torch.split(nbd_features, 1, dim=1))
-                
-        #         fr_queries_per_inst = list(zip(*fr_queries_per_scale))
-        #         for idx, clk_features in zip(insert_index, fr_queries_per_inst):
-        #             cat_features = torch.stack(clk_features, dim=-1)
-        #             fr_descriptors[idx] = torch.mean(cat_features, dim=-1)
-
-            
-        #     descriptors.append(fr_descriptors)
+        normalized_clicks = [torch.stack(clks).unsqueeze(0) for clks in normalized_clicks]
+        normalized_clicks = torch.cat(normalized_clicks, dim=0).to(device)
         
-        # descriptors = [torch.cat(desc, dim=1) for desc in descriptors]
-        
-        # return descriptors, normalized_clicks, num_queries_per_object.tolist()
+        return descriptors, normalized_clicks, num_queries_per_object
 
 
     def get_features_descriptors(self, fmap, point_coords_per_image):

dynamite_video/model/interactive_transformer/utils.py
@@ -60,12 +60,12 @@ class SelfAttentionLayer(nn.Module):
                      query_pos: Optional[Tensor] = None):
         q = k = self.with_pos_embed(tgt, query_pos)
 
-        tgt2 = self.self_attn(q, k, value=tgt, attn_mask=tgt_mask,
-                              key_padding_mask=tgt_key_padding_mask)[0]
+        tgt2, attn_wts = self.self_attn(q, k, value=tgt, attn_mask=tgt_mask,
+                              key_padding_mask=tgt_key_padding_mask)#[0]
         tgt = tgt + self.dropout(tgt2)
         tgt = self.norm(tgt)
 
-        return tgt
+        return tgt, attn_wts
 
     def forward_pre(self, tgt,
                     tgt_mask: Optional[Tensor] = None,
@@ -73,11 +73,11 @@ class SelfAttentionLayer(nn.Module):
                     query_pos: Optional[Tensor] = None):
         tgt2 = self.norm(tgt)
         q = k = self.with_pos_embed(tgt2, query_pos)
-        tgt2 = self.self_attn(q, k, value=tgt2, attn_mask=tgt_mask,
-                              key_padding_mask=tgt_key_padding_mask)[0]
+        tgt2, attn_wts = self.self_attn(q, k, value=tgt2, attn_mask=tgt_mask,
+                              key_padding_mask=tgt_key_padding_mask)#[0]
         tgt = tgt + self.dropout(tgt2)
 
-        return tgt
+        return tgt, attn_wts
 
     def forward(self, tgt,
                 tgt_mask: Optional[Tensor] = None,
@@ -135,15 +135,15 @@ class CrossAttentionLayer(nn.Module):
             query_pos: positional embedding for query, QxD
         """
         
-        tgt2 = self.multihead_attn(query=self.with_pos_embed(tgt, query_pos),
+        tgt2, attn_wts = self.multihead_attn(query=self.with_pos_embed(tgt, query_pos),
                                    key=self.with_pos_embed(memory, pos),
                                    value=memory, 
                                    attn_mask=memory_mask,
-                                   key_padding_mask=memory_key_padding_mask)[0]
+                                   key_padding_mask=memory_key_padding_mask)#[0]
         tgt = tgt + self.dropout(tgt2)
         tgt = self.norm(tgt)
        
-        return tgt
+        return tgt, attn_wts
 
     def forward_pre(
             self, 
@@ -169,14 +169,14 @@ class CrossAttentionLayer(nn.Module):
 
         tgt2 = self.norm(tgt)
        
-        tgt2 = self.multihead_attn(query=self.with_pos_embed(tgt2, query_pos),
+        tgt2, attn_wts = self.multihead_attn(query=self.with_pos_embed(tgt2, query_pos),
                                    key=self.with_pos_embed(memory, pos),
                                    value=memory, 
                                    attn_mask=memory_mask,
-                                   key_padding_mask=memory_key_padding_mask)[0]
+                                   key_padding_mask=memory_key_padding_mask)#[0]
         tgt = tgt + self.dropout(tgt2)
 
-        return tgt
+        return tgt, attn_wts
 
     
     def forward(

dynamite_video/model/loss/criterion.py
@@ -65,7 +65,7 @@ class SetFinalCriterion(nn.Module):
             for fr_idx, mask_pred in enumerate(outputs['pred_masks']):
                 H,W = mask_pred.shape[1:]
                 temp_out = []
-                splited_masks = torch.split(mask_pred, num_queries_per_object[fr_idx], dim=0)
+                splited_masks = torch.split(mask_pred, num_queries_per_object[fr_idx].tolist(), dim=0)
                 for m in splited_masks:
                     if len(m) == 0:
                         temp_out.append(torch.zeros(H,W).to(mask_pred.device))

dynamite_video/model/meta_arch/dynamite_head.py
@@ -76,8 +76,8 @@ class DynamiteHead(nn.Module):
             fg_coords = None, 
             bg_coords = None, 
             max_timestamp=None,
-            visualize=False,
-            train_iter=None
+            visualize=None,
+            train_iter=None,
     ):
         """
         Forward pass through DynaMITe segmentation head
@@ -98,16 +98,26 @@ class DynamiteHead(nn.Module):
         if (mask_features is None) or (multi_scale_features is None):
             mask_features, _, multi_scale_features = self.pixel_decoder.forward_features(features)
 
+        if visualize:
+            import os
+            import torch
+            visualize_dir = "/home/roy/REPOS/dynamite_video/debug/visualization/training/dynamite_head_px_decoder"
+            torch.save(mask_features, os.path.join(visualize_dir, f"mask_features_iter_{train_iter}.pth"))
+            torch.save(multi_scale_features, os.path.join(visualize_dir, f"multi_scale_features_iter_{train_iter}.pth"))
+
         if self.transformer_in_feature == "multi_scale_pixel_decoder":
             predictions, num_queries_per_object = self.interactive_transformer(data, 
-                                                                                                      images, 
-                                                                                                      objects_per_frame, 
-                                                                                                      multi_scale_features,
-                                                                                                      mask_features,
-                                                                                                      num_clicks_per_object,
-                                                                                                      fg_coords, 
-                                                                                                      bg_coords, 
-                                                                                                      max_timestamp)
+                                                                            images, 
+                                                                            objects_per_frame, 
+                                                                            multi_scale_features,
+                                                                            mask_features,
+                                                                            num_clicks_per_object,
+                                                                            fg_coords, 
+                                                                            bg_coords, 
+                                                                            max_timestamp,
+                                                                            visualize=visualize,
+                                                                            train_iter=train_iter,
+                                                                        )
         if self.training:
             return predictions, num_queries_per_object
         else:

dynamite_video/training/train_utils.py
@@ -59,10 +59,10 @@ def get_next_clicks(
     bg_coords,
     max_timestamp,
     num_objects_to_refine,
-    max_num_points=2,
+    max_num_points=1,
     visualize=False,
-    visualize_dir=None,
-    train_iter=None
+    train_iter=None,
+    round_num=None
 ):
     """
     Given the predicted masks of current round, sample corrective clicks
@@ -81,10 +81,20 @@ def get_next_clicks(
     """
 
     # directly take data as input as they are already on the device
-    gt_masks_clip = [x.cpu().numpy() for x in data["binary_masks"]]      # [T,N,H,W]
+    gt_masks_clip = [x.cpu().numpy() for x in data["binary_masks"]]        # [T,N,H,W]
     pred_masks_clip = [x.cpu().numpy() for x in pred_output]               # [T,N,H,W]
     semantic_maps_clip = [x.cpu().numpy() for x in data['semantic_masks']] # [T,H,W]
-    padding_mask = data["padding_mask"].cpu().numpy()                      # [H,W]
+    # ignore_masks_clip = [x.cpu().numpy().astype(np.uint8) for x in data['ignore_masks']]  # [T,H,W]
+    # padding_mask = data["padding_mask"].cpu().numpy()                      # [H,W]
+
+    # if visualize:
+    #     import os
+    #     visualize_dir = "/home/roy/REPOS/dynamite_video/debug/visualization/training/training_clicker"
+    #     torch.save(gt_masks_clip,       os.path.join(visualize_dir, f"gt_masks_round_{round_num}_iter_{train_iter}.pth"))
+    #     torch.save(pred_masks_clip,     os.path.join(visualize_dir, f"pred_masks_round_{round_num}_iter_{train_iter}.pth"))
+    #     torch.save(semantic_maps_clip,  os.path.join(visualize_dir, f"semantic_maps_round_{round_num}_iter_{train_iter}.pth"))
+    #     torch.save(ignore_masks_clip,   os.path.join(visualize_dir, f"ignore_masks_round_{round_num}_iter_{train_iter}.pth"))
+    #     torch.save(padding_mask,        os.path.join(visualize_dir, f"padding_mask_round_{round_num}_iter_{train_iter}.pth"))
     
     candidates_for_refinement = defaultdict(list)
     for fr_idx, (gt_masks, pred_masks) in enumerate(zip(gt_masks_clip, pred_masks_clip)):
@@ -95,18 +105,22 @@ def get_next_clicks(
             candidates_for_refinement[obj_id.item()].append(fr_idx)
 
     num_objects_to_refine = np.random.randint(1, min(len(candidates_for_refinement), num_objects_to_refine)+1)
+    # randomly select objects to be refined
     objects_to_refine = np.random.choice(list(candidates_for_refinement.keys()), num_objects_to_refine)
 
     for obj_id in objects_to_refine:
+        # randomly select a frame to sample clicks from
         fr_idx = np.random.choice(candidates_for_refinement[obj_id])
         gt_masks = gt_masks_clip[fr_idx]
         pred_masks = pred_masks_clip[fr_idx]
         semantic_map = semantic_maps_clip[fr_idx]
+        # ignore_mask = ignore_masks_clip[fr_idx]
 
         # timestamp of the latest click so far
         timestamp = max(max_timestamp)
-        sampled_clicks = _get_corrective_clicks(pred_masks[obj_id], gt_masks[obj_id], semantic_map, padding_mask,
-                                                    timestamp+1, 1, visualize, visualize_dir, f"{train_iter}_fr_{fr_idx}_obj_{obj_id}")
+        sampled_clicks = _get_corrective_clicks(pred_masks[obj_id], gt_masks[obj_id], semantic_map, 
+                                                    # ignore_mask, padding_mask,
+                                                    timestamp+1, max_num_points)
         
         if sampled_clicks is not None:
             for click in sampled_clicks:
@@ -152,12 +166,10 @@ def _get_corrective_clicks(
     pred_mask,
     gt_mask,
     semantic_map,
-    padding_mask,
+    # ignore_mask,
+    # padding_mask,
     timestamp,
     max_num_points=2,
-    visualize=False,
-    visualize_dir=None,
-    train_iter=None,
 ):
     """
     Sample corrective click on an object, in a frame
@@ -170,26 +182,20 @@ def _get_corrective_clicks(
         timestamp: timestamp of current click (int)
         max_num_points: maximum #clicks to sample (int, default: 2)
     """
-    import os
     gt_mask = np.asarray(gt_mask, dtype = np.bool_)
     pred_mask = np.asarray(pred_mask, dtype = np.bool_)
-    padding_mask = np.asarray(padding_mask, dtype = np.bool_)
-
-    if visualize:
-        torch.save(gt_mask, os.path.join(visualize_dir, f"ckpt_{train_iter}_gt_mask.pth"))
-        torch.save(pred_mask, os.path.join(visualize_dir, f"ckpt_{train_iter}_pred_mask.pth"))
+    # padding_mask = np.asarray(padding_mask, dtype = np.bool_)
+    # ignore_mask = np.asarray(ignore_mask, dtype = np.bool_)
     
     # negative error map - g.t. foreground missed by the prediction
     fn_mask =  np.logical_and(gt_mask, np.logical_not(pred_mask))
-    fn_mask = np.logical_and(fn_mask, np.logical_not(padding_mask))
+    # fn_mask = np.logical_and(fn_mask, np.logical_not(padding_mask))
+    # fn_mask = np.logical_and(fn_mask, np.logical_not(ignore_mask))
     
     # positive error map - g.t. background covered by the prediction
     fp_mask =  np.logical_and(np.logical_not(gt_mask), pred_mask)
-    fp_mask = np.logical_and(fp_mask, np.logical_not(padding_mask))
-
-    if visualize:
-        torch.save(fn_mask, os.path.join(visualize_dir, f"ckpt_{train_iter}_fn_mask.pth"))
-        torch.save(fp_mask, os.path.join(visualize_dir, f"ckpt_{train_iter}_fp_mask.pth"))
+    # fp_mask = np.logical_and(fp_mask, np.logical_not(padding_mask))
+    # fp_mask = np.logical_and(fp_mask, np.logical_not(ignore_mask))
     
     # distance transform to find the center of the error region
     fn_mask = np.pad(fn_mask, ((1, 1), (1, 1)), 'constant')
@@ -231,6 +237,4 @@ def _get_corrective_clicks(
             points_coords.append([coords[0], coords[1], obj_indx, timestamp])   # [y,x,i,t]
             timestamp+=1
 
-    if visualize:
-        torch.save(points_coords, os.path.join(visualize_dir, f"ckpt_{train_iter}_points_coords.pth"))
     return points_coords